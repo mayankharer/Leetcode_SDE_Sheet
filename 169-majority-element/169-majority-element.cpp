25c6d93f3a6906e8c607d75e62e5096c2adbd588
makh17 <91776967+makh17@users.noreply.github.com>
2022-06-07 16:55:58 +0530

    Time: 21 ms (78.55%), Space: 19.7 MB (13.92%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // Use of Hash Table storing the count 
-        int n  = nums.size();
-        unordered_map<int,int> counts;
-        for (auto v : nums) {
-            if(++counts[v] > n/2) return v;
+//         // Use of Hash Table storing the count 
+//         int n  = nums.size();
+//         unordered_map<int,int> counts;
+//         for (auto v : nums) {
+//             if(++counts[v] > n/2) return v;
+//         }
+//         return 0;
+        
+        //Moores voting algorithm
+        int count = 0;
+        int element = 0;
+        
+        for(int num:nums){
+            if(count==0){
+                element = num;
+            }
+            if(num == element) count+=1;
+            else count-=1;
         }
-        return 0;
+        return element;
     }
 };
