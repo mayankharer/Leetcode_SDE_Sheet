46c80eede23ea7fb45327d7ac85f1783e0e1eda0
makh17 <91776967+makh17@users.noreply.github.com>
2022-06-25 15:06:32 +0530

    Time: 2 ms (48.61%), Space: 6 MB (72.99%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -6,7 +6,7 @@ class Solution
 
            	// Using Combinatorics nCr most optimal solution
            	int N = n + m - 2;
-           	int r = m - 1; 
+           	int r = m - 1;  // or it can also be r = n - 1
            	double res = 1;
            	for (int i = 1; i <= r; i++)
            	    res = res *(N - r + i) / i;
