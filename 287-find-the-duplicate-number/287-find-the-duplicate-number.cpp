4cd8cf213bd45e028e2fb545e1fc742378c3299e
makh17 <91776967+makh17@users.noreply.github.com>
2022-07-12 01:11:11 +0530

    Time: 170 ms (62.64%), Space: 61.2 MB (91.03%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -11,22 +11,38 @@ public:
         // return ans[0];
         
         // optimised approach using binary search
-        int low = 1, high = nums.size() - 1, cnt;
-        while(low <=  high)
-        {
-            int mid = low + (high - low) / 2;
-            cnt = 0;
-            for(int n : nums)
-            {
-                if(n <= mid)
-                    ++cnt;
-            }
-            if(cnt <= mid)
-                low = mid + 1;
-            else
-                high = mid - 1;
+//         int low = 1, high = nums.size() - 1, cnt;
+//         while(low <=  high)
+//         {
+//             int mid = low + (high - low) / 2;
+//             cnt = 0;
+//             for(int n : nums)
+//             {
+//                 if(n <= mid)
+//                     ++cnt;
+//             }
+//             if(cnt <= mid)
+//                 low = mid + 1;
+//             else
+//                 high = mid - 1;
             
+//         }
+//         return low;
+//---------> Most optimised method Linked list cycle method
+// take 2 pointers one is slow which traverses each element of the vector once and fast pointer that skips one element and jumps to thid element 
+        int slow = nums[0];
+        int fast = nums[0];
+// the first loop runs until the slow and fast pointer does not meet or collide
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+// when they collide now set the fast pointer to first element and again traverse but now traverse at same pace   
+        fast = nums[0];
+        while(slow!=fast){
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return low;
+        return slow;        
     }
 };
