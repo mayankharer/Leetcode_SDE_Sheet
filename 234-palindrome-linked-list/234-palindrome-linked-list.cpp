844919aa737519ddf8566427d8ede8d8303fba10
makh17 <91776967+makh17@users.noreply.github.com>
2022-07-15 01:57:45 +0530

    Time: 317 ms (63.04%), Space: 118 MB (57.91%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -10,22 +10,73 @@
  */
 class Solution {
 public:
+    // ListNode* reverse(node* ptr) {
+    //     ListNode* pre=NULL;
+    //     ListNode* nex=NULL;
+    //     while(ptr!=NULL) {
+    //         nex = ptr->next;
+    //         ptr->next = pre;
+    //         pre=ptr;
+    //         ptr=nex;
+    //     }
+    //     return pre;
+    // }
+    
+    ListNode* reverse(ListNode*ptr){
+        ListNode* prev = NULL;
+        ListNode* next = NULL;
+        while(ptr){
+            next = ptr->next;
+            ptr->next = prev;
+            prev = ptr;  // increment the pointer
+            ptr = next; // increment the pointer
+        }
+        return prev;
+    }
+    
     bool isPalindrome(ListNode* head) {
-        ListNode * temp = head;
-        stack<int> st;
-        while(temp){
-            st.push(temp->val);
-            temp = temp->next;
+        //most optimal solution
+        //1->iterate and find the middle of the linked list
+        //2-> reverse the part after the linked list
+        //3->create a dummy node and now check if dummy->data == rev->data upto rev points NULL
+        
+        if(head==NULL||head->next==NULL) return true;
+        ListNode*slow = head;
+        ListNode*fast = head;
+        
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
         }
         
-        while(head){
-            int i = st.top();
-            st.pop();
-            if(head->val!=i) return false;
-            
-            head=head->next;
+        slow->next = reverse(slow->next);
+        slow = slow->next;
+        ListNode* dummy = head;
+        
+        while(slow!=NULL){
+            if(dummy->val != slow->val) return false;
+            dummy = dummy -> next;
+            slow = slow->next;
         }
         return true;
+        // ckecking using stack
+//         ListNode * temp = head;
+//         stack<int> st;
+//         while(temp){
+//             st.push(temp->val);
+//             temp = temp->next;
+//         }
+        
+//         while(head){
+//             int i = st.top();
+//             st.pop();
+//             if(head->val!=i) return false;
+            
+//             head=head->next;
+//         }
+//         return true;
+        
+    
     //     vector<int> ans;
     //     ListNode* p = head;
     //     while(p!=NULL){
