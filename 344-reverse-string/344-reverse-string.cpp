08e893ac06c4021b9796168b37aef3d6a7ab68f1
makh17 <91776967+makh17@users.noreply.github.com>
2022-07-15 14:21:22 +0530

    Time: 32 ms (58.01%), Space: 23.2 MB (76.25%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void swapp(char &x,char&y){
+        char temp;
+        temp = x ;
+        x = y;
+        y = temp;
+    }
+    
+    void reverseString(vector<char>& s) {
+        //  without using extra space
+        int l=0,h=s.size()-1;
+        for(int i = 0;i<s.size()/2;i++){
+            swapp(s[l],s[h]);
+            l++;
+            h--;
+        }
+        
+        //----------> by using extra space ---------------
+        // vector<char> ans;
+        // for(int i=s.size()-1;i>=0;i--) ans.emplace_back(s[i]);
+        // s = ans;
+    }
+};
