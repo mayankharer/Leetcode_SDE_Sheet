8649375cf713c31e61dacc9d0f4329ad305afa10
makh17 <91776967+makh17@users.noreply.github.com>
2022-07-19 14:14:22 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (71.06%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -5,13 +5,14 @@ public:
         vector<vector<int>> ans; 
         for(int i = 0 ;i < numRows ; i++ )
         {
-            vector<int>v(i+1,1);    //size of the vector is i+1 and every element is                                                   initialised to 1
-            for(int j = 1;j < i;j++){ 
-            //as all the vector elements are ini to 1 we know first and last element is 1 so                   we just traverse from 1 to j < i as vector is resized to i+1
+            vector<int>v(i+1,1);       //  size of the vector is i+1 and every element is                                                          initialised to 1
+            for(int j = 1;j < i;j++){  //  as all the vector elements are ini to 1 we know 
+                                       //  first and last element is 1 so we just traverse 
+                                       //  from 1 to j < i as vector is resized to i+1
                 
-                v[j]    =       ans[i-1][j]         +    ans[i-1][j-1];
-                    //       gives us the element      gives us the element    
-                    //       just above j              previos element above j
+            v[j]    =       ans[i-1][j]         +    ans[i-1][j-1];
+                    //  gives us the element      gives us the element    
+                    //  just above j              previos element above j
                 
             }
             ans.emplace_back(v);
