9b16f350fa74c74acc284f42e2d0ddbbb585d779
makh17 <91776967+makh17@users.noreply.github.com>
2022-07-25 20:30:05 +0530

    Time: 15 ms (61.79%), Space: 11.7 MB (36.94%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
@@ -12,16 +12,16 @@ class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head)
     {
-        ListNode* pointer = head;// create a pointer to iterate
-        while(pointer != NULL && pointer->next != NULL) //self explainatory
+        ListNode* temp = head;// create a pointer to iterate
+        while(temp != NULL && temp->next != NULL) //self explainatory
         {
-            if(pointer->val == pointer->next->val) // checking for consecutive elements
+            if(temp->val == temp->next->val) // checking for consecutive elements
             {
-                pointer->next = pointer->next->next; // point current pointer to next of next pointer
+                temp->next = temp->next->next; // point current pointer to next of next pointer
                 //if the val of current node is equals to the val of next node then skip the node
             }
             else
-                pointer = pointer->next;
+                temp = temp->next;
         }
         return head;
     }
