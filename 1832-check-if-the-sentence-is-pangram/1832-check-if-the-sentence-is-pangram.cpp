774062bbbba1c5273939b1bdb13a1a0a3cabdc0a
makh17 <91776967+makh17@users.noreply.github.com>
2022-08-11 18:11:59 +0530

    Time: 5 ms (43.38%), Space: 6.6 MB (27.51%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        vector<int> n(26,0);
+        
+        for(char c:sentence)n[c-'a']++;
+        
+        for(int i=0;i<26;i++)
+            if(n[i]==0)return false;
+        
+        return true;
+    }
+};
+
+// class Solution {
+// public:
+//     bool checkIfPangram(string s) {
+//         return set<char>(s.begin(), s.end()).size() == 26;
+//     }
+// };
