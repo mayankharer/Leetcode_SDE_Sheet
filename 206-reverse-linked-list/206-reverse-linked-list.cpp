46aac365beb6c13c052cf08e5e8577c3da687922
makh17 <91776967+makh17@users.noreply.github.com>
2022-08-19 00:34:25 +0530

    Time: 3 ms (98.40%), Space: 8.3 MB (42.11%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -8,23 +8,47 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         ListNode* newhead = NULL;
+//         while(head!=NULL){
+//             ListNode* next = head->next; //incrementing the next each time
+//             head->next = newhead; // pointing the pointer to prev that is newHead
+//             newhead = head; // as the newhead is pointing to NULL assign head as newHead
+//             head = next; // incrementing the head to next
+//         }
+//         return newhead;
+//     // }    
+//     }
+// };
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* newhead = NULL;
-        while(head!=NULL){
-            ListNode* next = head->next;
-            head->next = newhead;
-            newhead = head;
-            head = next;
+        ListNode* prevptr = NULL;
+        ListNode* currentptr = head;
+        // ListNode* nextptr = NULL;
+        ListNode* nextptr;
+        
+        while(currentptr!=NULL){
+            nextptr = currentptr->next;  
+            currentptr->next = prevptr;
+            //now incrementing the pointers
+            prevptr = currentptr;
+            currentptr = nextptr;
         }
-        return newhead;
-    }    
+        head = prevptr;
+        return head;
+    }
+};
+
+// class Solution{
 // public:
 //     void reverse(ListNode* &head){
 //         ListNode* prevptr = NULL;
 //         ListNode* currentptr = head;
-//         ListNode* nextptr = NULL;
+//         // ListNode* nextptr = NULL;
+//         ListNode* nextptr;
         
 //         while(currentptr!=NULL){
 //             nextptr = currentptr->next;  
@@ -34,12 +58,6 @@ public:
 //             currentptr = nextptr;
 //         }
 //         head = prevptr;
+//     return head;
 //     }
-    
-// public:
-//     ListNode* reverseList(ListNode* head) {
-//         reverse(head);
-//         return head;
-//         return 0;
-//     }
-};
+// };
