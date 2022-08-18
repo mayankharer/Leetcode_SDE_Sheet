002acfb39138c88b9b5a4f6e04a5c6e38656b863
makh17 <91776967+makh17@users.noreply.github.com>
2022-08-18 18:57:36 +0530

    Time: 157 ms (6.08%), Space: 31.2 MB (53.47%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -5,11 +5,13 @@ public:
         for(auto i:nums){
             sum+=i;
         }
+        cout << "sum is "<<sum<<"------------------\n";
         for(int i=0;i<nums.size();i++){
             if(prev_sum == sum-prev_sum-nums[i]){
                 return i;
             }
             prev_sum+=nums[i];
+            cout << prev_sum<<" "<<sum-prev_sum-nums[i]<<endl;
         }
         return -1;
         
