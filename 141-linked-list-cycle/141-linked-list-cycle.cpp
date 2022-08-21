9a2a3c94437f8bfc89769d4fd733ea0f7eb1abe7
makh17 <91776967+makh17@users.noreply.github.com>
2022-08-21 22:32:49 +0530

    Time: 28 ms (14.06%), Space: 11.3 MB (7.72%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -6,18 +6,43 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head == NULL) return false;
-        ListNode* fast = head;
-        ListNode* slow = head;
-        
-        while(fast->next != NULL && fast->next->next != NULL) {
-            fast = fast->next->next;
-            slow = slow->next;
-            if(fast == slow) return true;
+        if(head==NULL) return false;
+        unordered_map<ListNode*,int> mp;
+        while(head!=NULL){
+            if(mp.count(head)>0)
+                return true;
+            else
+                mp[head] = 1;
+            head = head->next;
         }
         return false;
     }
 };
+//--------------------------------------------------------------------
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode(int x) : val(x), next(NULL) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         if(head == NULL) return false;
+//         ListNode* fast = head;
+//         ListNode* slow = head;
+        
+//         while(fast->next != NULL && fast->next->next != NULL) {
+//             fast = fast->next->next; // traverse fast by two nodes 
+//             slow = slow->next;// traverse slow by one node at a time
+//             if(fast == slow) return true; 
+//         }
+//         return false;
+//     }
+// };
