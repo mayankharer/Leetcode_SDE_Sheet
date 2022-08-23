d29cffb351b5334e84b08ae5100d1d20f236e371
makh17 <91776967+makh17@users.noreply.github.com>
2022-08-23 22:59:17 +0530

    Time: 8 ms (86.90%), Space: 7.6 MB (75.53%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -9,14 +9,14 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        if(head==NULL)return NULL;
+        if(head == NULL) return NULL;
         ListNode* slow = head;
         ListNode* fast = head;
         bool isCycle = false;
         while(fast->next!=NULL && fast->next->next!=NULL){
-            fast = fast->next->next;
             slow = slow->next;
-            if(fast == slow) {
+            fast = fast->next->next;
+            if(slow == fast){
                 isCycle = true;
                 break;
             }
@@ -26,10 +26,62 @@ public:
         
         slow = head;
         while(slow!=fast){
-	    	slow = slow -> next;
-		    fast = fast -> next;
-    	}
+            slow = slow->next;
+            fast = fast->next;
+        }
+        return slow;
+        
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        
+        
+        
+        
+//        if(head==NULL)return NULL;
+//         ListNode* slow = head;
+//         ListNode* fast = head;
+//         bool isCycle = false;
+//         while(fast->next!=NULL && fast->next->next!=NULL){
+//             fast = fast->next->next;
+//             slow = slow->next;
+//             if(fast == slow) {
+//                 isCycle = true;
+//                 break;
+//             }
+//         }
+//         // if there is no cycle in the LL return NULL
+//         if(!isCycle) return NULL;
+//         // after this loop we find the point at which bith the pointers meet
+//         // now keep the fast pointer thereitself and move slow pointer to head
+//         // now traverse slow and fast by one step each and point
+//         // the point at which they meet each other is starting point of LOOP
+        
+        
+//         slow = head;
+//         while(slow!=fast){
+// 	    	slow = slow -> next;
+// 		    fast = fast -> next;
+//     	}
 
-	    return fast;
+// 	    return fast;
     }
 };
