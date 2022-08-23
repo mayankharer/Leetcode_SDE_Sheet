f5c620b9c9b14761988de15e0270d1a2e2a57694
makh17 <91776967+makh17@users.noreply.github.com>
2022-08-23 22:27:39 +0530

    Time: 12 ms (56.87%), Space: 14.9 MB (13.87%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -14,55 +14,51 @@ public:
         //optimised space complexity
         // when list1 is empty then 
         // our output will be same as list2
-        if(l1 == NULL) return l2; 
+//         if(l1 == NULL) return l2; 
 
-        // when list2 is empty then our output 
-        // will be same as list1
-        if(l2 == NULL) return l1;
+//         // when list2 is empty then our output 
+//         // will be same as list1
+//         if(l2 == NULL) return l1;
 
-        // pointing l1 and l2 to smallest and greatest one
-        if(l1->val > l2->val) std::swap(l1,l2);
+//         // pointing l1 and l2 to smallest and greatest one
+//         if(l1->val > l2->val) std::swap(l1,l2);
 
-        // act as head of resultant merged list
-        ListNode* res = l1;
+//         // act as head of resultant merged list
+//         ListNode* res = l1;
 
-        while(l1 != NULL && l2 != NULL) {
+//         while(l1 != NULL && l2 != NULL) {
  
-            ListNode* temp = NULL;
+//             ListNode* temp = NULL;
 
-            while(l1 != NULL && l1->val <= l2->val) {
+//             while(l1 != NULL && l1->val <= l2->val) {
 
-                temp = l1;//storing last sorted node  
-                l1 = l1->next;
-            }
+//                 temp = l1;//storing last sorted node  
+//                 l1 = l1->next;
+//             }
 
-            // link previous sorted node with 
-            // next larger node in list2
-            temp->next = l2;
-            std::swap(l1,l2);
-        }
-        return res;
+//             // link previous sorted node with 
+//             // next larger node in list2
+//             temp->next = l2;
+//             std::swap(l1,l2);
+//         }
+//         return res;
         
         // // first  solution by using extra space i.e creating new LL 
-        // ListNode *temp = new ListNode(); // create a dummy node or temperory node
-        // ListNode *curr = temp;           // node for traversing the as per the smaller value LL
-        // while(list1!=NULL && list2!=NULL){ // we travrse until any of the LL points to null
-        //     if(list1->val <= list2->val ){
-        //         curr->next = list1;
-        //         list1 = list1->next;
-        //     }
-        //     else{
-        //         curr->next = list2;
-        //         list2 = list2->next;
-        //     }
-        //     curr = curr->next;
-        // }
-        // curr->next = list1 != NULL ? list1:list2;
+        ListNode *temp = new ListNode(); // create a dummy node or temperory node
+        ListNode *curr = temp;           // node for traversing the as per the smaller value LL representing temp LL
+        while(l1!=NULL && l2!=NULL){ // we travrse until any of the LL points to null
+            if(l1->val <= l2->val ){ // comparing the value if the nodes that are pointing
+                curr->next = l1; //insert the smaller ll to next of current  
+                l1 = l1->next;// traverse forward
+            }
+            else{
+                curr->next = l2;
+                l2 = l2->next;
+            }
+            curr = curr->next; // move the current to next
+        }
+        curr->next = l1 != NULL ? l1:l2;
         // the above line states that whenever any ll finishes or points to NULL we 
-        // return temp->next;
-        
-        // w/o using extra space 
-        
+        return temp->next;
     }
 };
-
