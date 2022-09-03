09e6a2b82bc8fa94f8d4925579de4b2e087e1110
makh17 <91776967+makh17@users.noreply.github.com>
2022-09-03 17:45:35 +0530

    Time: 7 ms (14.98%), Space: 5.9 MB (27.33%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
-    bool isPowerOfTwo(int n) {
-	    if(n <= 1) return n == 1; //base condition
-	    return n % 2 == 0 && isPowerOfTwo(n / 2);
+    bool isPowerOfTwo(long n) {
+	    // using bit manipulation
+        return (n && !( n & (n-1)));
+        
+        
+        // ------- Recursive solution-----------
+        // if(n <= 1) return n == 1; //base condition
+	    // return n % 2 == 0 && isPowerOfTwo(n / 2);
     }
 };
